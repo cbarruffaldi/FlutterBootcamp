491b7f48993642c8d9eb9c92e77da7f21768920d
Carla Barruffaldi <cbarruffaldi@itba.edu.ar>
2019-11-15 15:20:52 +0100

    Magic 8 Ball finish

--- magic-8-ball/lib/main.dart
+++ magic-8-ball/lib/main.dart
@@ -1,7 +1,44 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.blue,
+          appBar: AppBar(
+            title: Text('Ask Me Anything'),
+            backgroundColor: Colors.indigo,
+          ),
+          body: eightBall(),
+        ),
       ),
     );
+
+class eightBall extends StatefulWidget {
+  @override
+  _eightBallState createState() => _eightBallState();
+}
+
+class _eightBallState extends State<eightBall> {
+  int ballNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  ballNumber = Random().nextInt(5) + 1;
+                });
+              },
+              child: Image.asset('images/ball$ballNumber.png'),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
