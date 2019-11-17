b1f2209d1c2f902b33a57d4154a23419c3239a43
Carla Barruffaldi <cbarruffaldi@itba.edu.ar>
2019-11-17 17:20:52 +0100

    BMI Calculator end

---
+++ bmi-calculator-flutter/lib/components/button_bottom.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import '../constants.dart';
+
+class BottomButton extends StatelessWidget {
+  final Function onTap;
+  final String bottomTitle;
+
+  BottomButton({this.onTap, this.bottomTitle});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        child: Center(
+          child: Text(
+            bottomTitle,
+            style: kLargeButtonTextStyle,
+          ),
+        ),
+        color: kBottomContainerColor,
+        margin: EdgeInsets.only(top: 10.0),
+        padding: EdgeInsets.only(bottom: 20.0),
+        width: double.infinity,
+        height: kBottomContainerHeight,
+      ),
+    );
+  }
+}
