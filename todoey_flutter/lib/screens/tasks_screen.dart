35fd506a2bf61ee0c8b1256b3ee35d8610ad3a97
Carla Barruffaldi <cbarruffaldi@itba.edu.ar>
2020-02-25 17:12:01 +0100

    Using Provider Package

--- todoey_flutter/lib/screens/tasks_screen.dart
+++ todoey_flutter/lib/screens/tasks_screen.dart
@@ -1,21 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:todoey_flutter/model/task.dart';
+import 'package:todoey_flutter/model/task_data.dart';
 import 'package:todoey_flutter/widgets/tasks_list.dart';
+import 'package:provider/provider.dart';
 
 import 'add_task_screen.dart';
 
-class TasksScreen extends StatefulWidget {
-  @override
-  _TasksScreenState createState() => _TasksScreenState();
-}
-
-class _TasksScreenState extends State<TasksScreen> {
-  List<Task> tasks = [
-    Task(name: 'Buy Milk'),
-    Task(name: 'Buy eggs'),
-    Task(name: 'Buy bread'),
-  ];
-
+class TasksScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,9 +24,9 @@ class _TasksScreenState extends State<TasksScreen> {
                   padding: EdgeInsets.only(
                       bottom: MediaQuery.of(context).viewInsets.bottom),
                   child: AddTaskScreen(addTaskCallback: (newTaskTitle) {
-                    setState(() {
-                      tasks.add(new Task(name: newTaskTitle));
-                    });
+//                    setState(() {
+//                      tasks.add(new Task(name: newTaskTitle));
+//                    });
                     Navigator.pop(context);
                   }),
                 ),
@@ -73,7 +64,7 @@ class _TasksScreenState extends State<TasksScreen> {
                     ),
                   ),
                   Text(
-                    '${tasks.length} Tasks',
+                    '${Provider.of<TaskData>(context).taskCount} Tasks',
                     style: TextStyle(
                       color: Colors.white,
                       fontSize: 18.0,
@@ -92,7 +83,7 @@ class _TasksScreenState extends State<TasksScreen> {
                     topRight: Radius.circular(20.0),
                   ),
                 ),
-                child: TasksList(tasks),
+                child: TasksList(),
               ),
             ),
           ],
